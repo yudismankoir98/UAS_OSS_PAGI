4c46ea41f583e360b37ea06eec2e66a972833193
yudismankoir98 <49525546+yudismankoir98@users.noreply.github.com>
2019-06-24 12:54:22 +0700

    Bryan Mochammad Yudhistira_G.211.17.0039k_UAS_OSS_PAGI.txt

--- UAS.py
+++ UAS.py
@@ -1,25 +1,27 @@
-def Penjumlahan():
+@@ -2,21 +2,21 @@ def Penjumlahan():
 	hasil=500+200
 	return hasil
-  
+
 def Penjumlahan(args):
+def Penjumlahan(*args):
 	hasil=500+200
 	return hasil
-  
+
 def Pengurangan():
 	hasil=700-190
 	return hasil
-  
+
 def Pengurangan(args):
+def Pengurangan(*args):
 	hasil=700-190
 	return hasil
-  
+
 def Tampil(func,func1):
 	print "Anto memiliki bebek "func()" ekor di Mataram"
 	print "Bebek anto pada tahun 2013 menjadi "func1()" karena mati keracunan"
+	print "Anto memiliki bebek ",func()," ekor di Mataram"
+	print "Bebek anto pada tahun 2013 menjadi ",func1()," karena mati keracunan"
 
 def Utama():
 	Tampil(Penjumlahan,Pengurangan)
-
-#pemanggilan fungsi Utama
-Utama()
+	
